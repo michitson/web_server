9b7b08c5aacce290d70483e306f46e4b9319db9f
Andrew Michitson <michitson@yahoo.com>
2019-12-26 22:59:30 +0000

    attempt to fix  port problem in app.js

--- src/app.js
+++ src/app.js
@@ -86,7 +86,8 @@ app.get('*', (req, res) => {
     })
 })
 
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is running on port 3000')
 })
